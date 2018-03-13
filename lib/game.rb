b17f21c1e7934815c4985f1aacb3a6eccdd95f7c
Eden Mesfun <emesfun@gmail.com>
2018-03-13 15:09:55 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -61,6 +61,7 @@ class Game < Player
   end
 
   def play
+<<<<<<< HEAD
     until over?
       turn
     end
@@ -68,6 +69,14 @@ class Game < Player
         puts "Congratulations #{winner}!"
       elsif draw?
         puts "Cat's Game!"
+=======
+        turn
+      while over? == false
+        self.board.update(self.current_player.token, self.current_player)
+        turn
+        over?
+        break if won? == true || over? == true
+>>>>>>> 721901e59f3b6d389e1b9a87a1be40b798febbae
       end
   end
 
